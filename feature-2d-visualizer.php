a9eb578915a54aca1e7c9e6630fb2d9005460054
Cholene Jane Aberin <144436690+cholenejane@users.noreply.github.com>
2025-08-04 18:01:30 +0800

    feature: Add feature-2d-visualizer.php feature Update index.php

---
+++ feature-2d-visualizer.php
@@ -0,0 +1,44 @@
+<?php
+include 'includes/header.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rich Anne Lea Tiles Trading - Tile Visualizer</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.17.2/dist/sweetalert2.min.js"></script>
+    <!-- <link rel="stylesheet" href="style.css"> -->
+    <style>
+        /* Base Styles */
+        * {
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+        }
+        
+        body {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+            font-family: 'Inter', sans-serif;
+            color: #333;
+            line-height: 1.6;
+            overflow-x: hidden;
+        }
+    </style>
+</head>
+
+<body>
+
+    <h1>jane</h1>
+</body>
+</html>
+<?php
+include 'includes/footer.php';
+?>
