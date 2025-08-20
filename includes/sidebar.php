0be049dbff545f706c6b57fe289c65ca20c3210f
Gyan Gee Almanon <153313413+almanongeeyan@users.noreply.github.com>
2025-08-20 22:44:06 +0800

    changes

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Staff Sidebar</title>
+    <title>Staff Admin Page</title>
     <style>
         :root {
             /* Updated color scheme */
